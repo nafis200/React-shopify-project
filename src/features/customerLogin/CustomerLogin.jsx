71d8de826d650649a4799a47bdffa8a840057285
nafis200 <nafisahamed14@gmail.com>
2024-10-12 14:56:46 +0600

    animated view

--- src/features/customerLogin/CustomerLogin.jsx
+++ src/features/customerLogin/CustomerLogin.jsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, Animated, Image, TouchableOpacity,Alert } from "react-native";
-import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, Animated, Image, TouchableOpacity, Alert } from "react-native";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { GestureHandlerRootView, PanGestureHandler, State } from "react-native-gesture-handler";
 import CustomSafeAreaView from "../../components/CustomSafeAreaView.jsx/CustomSafeAreaView";
 import ProductSlider from "../../components/login/ProductSlider";
@@ -11,6 +11,7 @@ import { AuthContext } from "../../provider/Authprovider";
 import { NavigationContainer } from '@react-navigation/native';
 import useAxiospublic from "../../provider/hooks/useAxiospublic";
 import { useQuery } from "@tanstack/react-query";
+import useKeyboardHeight from "../../utils/usekeyboard";
 
 
 const CustomerLogin = () => {
@@ -19,41 +20,60 @@ const CustomerLogin = () => {
   const [loading, setLoading] = useState(false)
   const [gestureSequence, setGestureSequence] = useState([])
 
-  const { signInUser,user} = useContext(AuthContext)
+  const keyboardHeight = useKeyboardHeight()
 
-  
-  
-  const axiosPublic = useAxiospublic()
-  
-  const { data: users = []} = useQuery({
-    queryKey: ['users',user?.email],
-    queryFn: async () => {
-        const res = await axiosPublic.get(`/users/${user?.email}`); // Fetch all users
-        return res.data;
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (keyboardHeight === 0) {
+      Animated.timing(animatedValue, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(animatedValue, {
+        toValue: -keyboardHeight * 0.84,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
     }
-});
-  
- console.log(users.role);
- 
- 
- 
+  }, [keyboardHeight]);
+
+
+  const { signInUser, user } = useContext(AuthContext)
+
+
+
+  const axiosPublic = useAxiospublic()
+
+  const { data: users = [] } = useQuery({
+    queryKey: ['users', user?.email],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/users/${user?.email}`); // Fetch all users
+      return res.data;
+    }
+  });
+
+
+
 
   const handleLogin = async () => {
     try {
       await signInUser(Email, password);
       Alert.alert(
         "Alert Title",
-        "Login successfully", 
+        "Login successfully",
         [
-          { text: "OK", onPress: () => console.log("OK Pressed") } 
+          { text: "OK", onPress: () => console.log("OK Pressed") }
         ]
       );
     } catch (error) {
       Alert.alert(
         "Alert Title",
-        "Email password dont match", 
+        "Email password dont match",
         [
-          { text: "OK", onPress: () => console.log("OK Pressed") } 
+          { text: "OK", onPress: () => console.log("OK Pressed") }
         ]
       );
     }
@@ -98,6 +118,7 @@ const CustomerLogin = () => {
               keyboardDismissMode='on-drag'
               keyboardShouldPersistTaps='handled'
               contentContainerStyle={styles.subContainer}
+              style={{ transform: [{ translateY: animatedValue }] }}
             >
               <View style={styles.content}>
                 <Image source={require('../../assets/image1/01.png')} style={styles.logo} />
@@ -132,11 +153,11 @@ const CustomerLogin = () => {
                   loading={loading}
                   onPress={() => handleLogin()}
                 />
-                <View style={{flexDirection:'row'}}>
-                <Text style={{ color: 'black', marginRight: 10 }}>Have not any Accout Please</Text>
-                <TouchableOpacity onPress={() => resetAndNavigate('Register')}>
-                  <Text style={styles.link}>Register</Text>
-                </TouchableOpacity>
+                <View style={{ flexDirection: 'row' }}>
+                  <Text style={{ color: 'black', marginRight: 10 }}>Have not any Accout Please</Text>
+                  <TouchableOpacity onPress={() => resetAndNavigate('Register')}>
+                    <Text style={styles.link}>Register</Text>
+                  </TouchableOpacity>
                 </View>
 
               </View>
