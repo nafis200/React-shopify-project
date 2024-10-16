721e98b07dd5d9793757bd5b0922a70ca70a1a63
nafis200 <nafisahamed14@gmail.com>
2024-10-17 01:06:44 +0600

    raksha bondon

---
+++ src/components/ui/CustomHeader.jsx
@@ -0,0 +1,36 @@
+
+
+import { StyleSheet, Text,SafeAreaView,View, Pressable } from "react-native";
+import React from "react";
+import Icon from 'react-native-vector-icons/Ionicons'
+import { RFValue } from "react-native-responsive-fontsize";
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.flexRow}>
+         <Pressable>
+            <Icon name='chevron-back' color={'black'} size={RFValue(16)} />
+         </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default CustomHeader;
+
+const styles = StyleSheet.create({
+    flexRow:{
+        justifyContent:'space-between',
+        padding:10,
+        height:60,
+        flexDirection:'row',
+        alignItems:'center',
+        backgroundColor:'white',
+        borderBottomWidth:0.6
+    },
+    text:{
+        textAlign:'center',
+        color:'black'
+    }
+});
--- src/features/category/ProductCategory.jsx
+++ src/features/category/ProductCategory.jsx
@@ -1,16 +1,38 @@
 
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import CustomHeader from "components/ui/CustomHeader";
+
 
 const ProductCategory = () => {
+  const [categories,SetCategories] = useState([])
+  const [selectedCategory,SetSelectedCategory] = useState(null)
+  const [products,SetProducts] = useState([])
+  const [categoriesLoading,SetCategoriesLoading] = useState(true)
+  const [productsLoading,SetProductsLoading] = useState(false)
   return (
-    <View>
-      <Text>ProductCategory</Text>
+    <View style={styles.mainContainer}>
+      <CustomHeader title={selectedCategory?.name || "categories"} search />
     </View>
   );
 };
 
 export default ProductCategory;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer:{
+      flex:1,
+      backgroundColor:'white'
+  },
+  subContainer:{
+      flex:1,
+      flexDirection:'row',
+      alignItems:'center'
+  },
+  center:{
+     flex:1,
+     justifyContent:'center',
+     alignItems:'center'
+  }
+});
    
