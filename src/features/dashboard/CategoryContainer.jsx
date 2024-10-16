fa91dcd9b926ca64d2ee00dfb912e217a309f8ef
nafis200 <nafisahamed14@gmail.com>
2024-10-16 14:49:38 +0600

    card is done

--- src/features/dashboard/CategoryContainer.jsx
+++ src/features/dashboard/CategoryContainer.jsx
@@ -3,23 +3,51 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAxiospublic from "provider/hooks/useAxiospublic";
+import Scalepress from "components/ui/Scalepress";
+import { navigate } from "utils/NavigationUtils";
+import { Image } from "react-native-svg";
+import CustomText from "components/ui/CustomText";
 
 const CategoryContainer = () => {
-    const axiosPublic = useAxiospublic();
-    const { data: users = [],isLoading,refetch} = useQuery({
-        queryKey: ['products'],
-        queryFn: async () => {
-          const res = await axiosPublic.get(`products`);
-          return res.data;
-        },
-      });
-      if(isLoading){
-        return <Text style={{color:'black'}}>Loading......</Text>
-      }
-      console.log(users)
+  const axiosPublic = useAxiospublic();
+  const { data: Data = [], isLoading, refetch } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`products`);
+      return res.data;
+    },
+  });
+  if (isLoading) {
+    return <Text style={{ color: 'black' }}>Loading......</Text>
+  }
+
+  const renderItems = (items) => {
+    return (
+      <>
+        {items.map((item, index) => {
+          return (
+            <Scalepress onPress={() => navigate('ProductCategory')} key={index} style={styles.item} >
+              <View style={styles.imageContainer} >
+                 <Image source={item.image} style={styles.image} />
+              </View>
+              <CustomText style={styles.text} variant="h8">{item.product_name}</CustomText>
+            </Scalepress>
+          );
+        })}
+      </>
+    );
+  };
+
+
+
   return (
-    <View>
-      <Text>CategoryContainer</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        {renderItems(Data?.slice(0,4))}
+      </View>
+      <View style={styles.row}>
+      {renderItems(Data?.slice(4,8))}
+      </View>
     </View>
   );
 };
@@ -27,13 +55,37 @@ const CategoryContainer = () => {
 export default CategoryContainer;
 
 const styles = StyleSheet.create({
-    container:{
-        marginVertical:15
-    },
-    row:{
-       flexDirection:'row',
-       justifyContent:'space-between',
-       alignItems:'baseline',
-       marginBottom:25
-    }
+  container: {
+    marginVertical: 15
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    marginBottom: 25
+  },
+  imageContainer:{
+      width:'100%',
+      height:80,
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:10,
+      padding:6,
+      backgroundColor:'#E5F3F3',
+      marginBottom:8
+  },
+  item:{
+      width:'22%',
+      justifyContent:'center',
+      alignItems:'center'
+  },
+  text:{
+      textAlign:'center',
+      color:'black'
+  },
+  image:{
+     width:'100%',
+     height:'100%',
+     resizeMode:'contain'
+  }
 });
