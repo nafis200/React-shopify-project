c2231d7b1fdaecde36faa4038b8f0e06507f5caa
nafis200 <nafisahamed14@gmail.com>
2024-10-17 01:11:18 +0600

    product categories

--- src/components/ui/CustomHeader.jsx
+++ src/components/ui/CustomHeader.jsx
@@ -4,14 +4,20 @@ import { StyleSheet, Text,SafeAreaView,View, Pressable } from "react-native";
 import React from "react";
 import Icon from 'react-native-vector-icons/Ionicons'
 import { RFValue } from "react-native-responsive-fontsize";
+import { goBack } from "utils/NavigationUtils";
+import CustomText from "./CustomText";
 
-const CustomHeader = () => {
+const CustomHeader = ({title,search}) => {
   return (
     <SafeAreaView>
       <View style={styles.flexRow}>
-         <Pressable>
+         <Pressable onPress={()=> goBack()}>
             <Icon name='chevron-back' color={'black'} size={RFValue(16)} />
          </Pressable>
+         <CustomText style={styles.text} variant="h5" >{title}</CustomText>
+         <View>
+            {search && <Icon name='search' size={RFValue(16)} color='black' /> }
+         </View>
       </View>
     </SafeAreaView>
   );
