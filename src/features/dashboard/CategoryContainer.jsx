ad27999ce1677a62fa5ffd3ffc197a9a88a2c8f1
nafis200 <nafisahamed14@gmail.com>
2024-10-16 12:56:10 +0600

    well done is data is comming

---
+++ src/features/dashboard/CategoryContainer.jsx
@@ -0,0 +1,29 @@
+
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import useAxiospublic from "provider/hooks/useAxiospublic";
+
+const CategoryContainer = () => {
+    const axiosPublic = useAxiospublic();
+    const { data: users = [],isLoading,refetch} = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+          const res = await axiosPublic.get(`products`);
+          return res.data;
+        },
+      });
+      if(isLoading){
+        return <Text style={{color:'black'}}>Loading......</Text>
+      }
+      console.log(users)
+  return (
+    <View>
+      <Text>CategoryContainer</Text>
+    </View>
+  );
+};
+
+export default CategoryContainer;
+
+const styles = StyleSheet.create({});
--- src/features/dashboard/ContentContainer.jsx
+++ src/features/dashboard/ContentContainer.jsx
@@ -4,11 +4,15 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import AdCarousel from "./AdCarousel";
 import { imageData } from './../../Dummydata/Dummydata';
+import CustomText from "components/ui/CustomText";
+import CategoryContainer from "./CategoryContainer";
 
 const ContentContainer = () => {
   return (
     <View style={styles.container}>
         <AdCarousel imageData={imageData} />
+        <CustomText variant="h5" style={{color:'black'}} >Grocery & Kitchen</CustomText>
+        <CategoryContainer/>
     </View>
   );
 };
