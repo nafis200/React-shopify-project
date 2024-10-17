1e580afb58e4a150997c13d59d802abef3436d37
nafis200 <nafisahamed14@gmail.com>
2024-10-17 19:19:27 +0600

    storage is made

---
+++ src/state/cartStore.jsx
@@ -0,0 +1,67 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+export const useCartStore = create(
+  persist(
+    (set, get) => ({
+      cart: [],
+
+      addItem: (item) => {
+        const currentCart = get().cart;
+        const existingItemIndex = currentCart.findIndex(cartItem => cartItem._id === item._id);
+
+        if (existingItemIndex >= 0) {
+          const updatedCart = [...currentCart];
+          updatedCart[existingItemIndex] = {
+            ...updatedCart[existingItemIndex],
+            count: updatedCart[existingItemIndex].count + 1
+          };
+          set({ cart: updatedCart });
+        } else {
+          set({
+            cart: [...currentCart, { _id: item._id, item: item, count: 1 }]
+          });
+        }
+      },
+
+      clearCart: () => set({ cart: [] }),
+
+      removeItem: (id) => {
+        const currentCart = get().cart;
+        const existingItemIndex = currentCart.findIndex(cartItem => cartItem._id === id);
+
+        if (existingItemIndex >= 0) {
+          const updatedCart = [...currentCart];
+          const existingItem = updatedCart[existingItemIndex];
+
+          if (existingItem.count > 1) {
+            updatedCart[existingItemIndex] = {
+              ...existingItem,
+              count: existingItem.count - 1
+            };
+          } else {
+            updatedCart.splice(existingItemIndex, 1); // Remove item if count is 0
+          }
+
+          set({ cart: updatedCart });
+        }
+      },
+
+      getItemCount: (id) => {
+        const currentItem = get().cart.find(cartItem => cartItem._id === id);
+        return currentItem ? currentItem.count : 0;
+      },
+
+      getTotalPrice: () => {
+        return get().cart.reduce((total, cartItem) => {
+          return total + cartItem.item.price * cartItem.count;
+        }, 0);
+      }
+    }),
+    {
+      name: 'cart-storage',
+      storage: createJSONStorage(() => AsyncStorage)
+    }
+  )
+);
