a9036a9d585e2a423576bd8e155ad7497aff95b4
nafis200 <nafisahamed14@gmail.com>
2024-10-17 23:17:31 +0600

    btn width

--- src/features/cart/Cartsummary.jsx
+++ src/features/cart/Cartsummary.jsx
@@ -1,15 +1,45 @@
 
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { screenHeight, screenWidth } from "utils/Scaling";
 
-const Cartsummary = () => {
+const Cartsummary = ({cartCount,cartImage}) => {
   return (
-    <View>
-      <Text>Cartsummary</Text>
+    <View style={styles.container}>
+      <Text style={{color:'black'}}>Cartsummary</Text>
     </View>
   );
 };
 
 export default Cartsummary;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+   container:{
+     justifyContent:'space-between',
+     alignItems:'center',
+     flexDirection:'row',
+     paddingHorizontal:screenHeight * 0.05,
+     paddingBottom:screenHeight * 0.03,
+     paddingTop:screenHeight * 0.014
+   },
+
+   flexRowGap:{
+     alignItems:'center',
+     flexDirection:'row',
+     gap:screenWidth * 0.03,
+   },
+   image:{
+      width:screenWidth * 0.1,
+      height:screenWidth * 0.1,
+      borderRadius: screenWidth * 0.025,
+      borderWidth:1 
+   },
+   btn:{
+      flexDirection:'row',
+      alignItems:'center',
+      justifyContent:'center',
+      paddingVertical:screenHeight * 0.01,
+      borderRadius: screenWidth * 0.025,
+      paddingHorizontal:screenWidth * 0.1
+   }
+});
