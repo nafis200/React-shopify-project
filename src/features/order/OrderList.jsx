0aa36d1b40bbb5f2535d75a89300c2338c0251ba
nafis200 <nafisahamed14@gmail.com>
2024-10-18 14:49:11 +0600

    cyclone dana

---
+++ src/features/order/OrderList.jsx
@@ -0,0 +1,53 @@
+
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useCartStore } from "state/cartStore";
+import CustomText from "components/ui/CustomText";
+
+const OrderList = () => {
+    const cartItem = useCartStore(state => state.cart)
+    return (
+        <View style={styles.container}>
+            <View style={styles.flexRow}>
+
+                <View style={styles.imgContainer}>
+                    <Image source={require('@assets/icons/clock.png')} />
+                </View>
+
+                <View>
+                    <CustomText style={styles.text} variant="h8">Delivery in 8 min</CustomText>
+                    <CustomText style={styles.text} variant="h8">Shipment of {cartItem?.length || 0} item </CustomText>
+                </View>
+
+            </View>
+        </View>
+    );
+};
+
+export default OrderList;
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'black'
+    },
+    container: {
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        marginBottom: 15
+    },
+    imgContainer: {
+         padding:10,
+         borderRadius:15
+    },
+    flexRow: {
+    alignItems:'center',
+    flexDirection:'row',
+    gap:12,
+    paddingHorizontal:10,
+    paddingVertical:12
+    },
+    img:{
+       width:30,
+       height:30
+    } 
+});
--- src/features/order/Productorder.jsx
+++ src/features/order/Productorder.jsx
@@ -1,15 +1,35 @@
 
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import CustomHeader from "components/ui/CustomHeader";
+import OrderList from "./OrderList";
 
 const Productorder = () => {
   return (
     <View>
-      <Text>Productorder</Text>
+      <CustomHeader title="checkout" />
+      <ScrollView contentContainerStyle={styles.scrollContainer} >
+         <OrderList/>
+      </ScrollView>
     </View>
   );
 };
 
 export default Productorder;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#fff'
+    },
+    scrollContainer:{
+        backgroundColor:'silver',
+        padding:10,
+        paddingBottom: 250
+    },
+    text:{
+        color:'black'
+    }
+});
+
+
