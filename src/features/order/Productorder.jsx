bada6be99716a212afe27d0ab83710a09f549f94
nafis200 <nafisahamed14@gmail.com>
2024-10-18 15:25:40 +0600

    git push

--- src/features/order/Productorder.jsx
+++ src/features/order/Productorder.jsx
@@ -1,15 +1,27 @@
 
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View,Image } from "react-native";
 import React from "react";
 import CustomHeader from "components/ui/CustomHeader";
 import OrderList from "./OrderList";
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { RFValue } from "react-native-responsive-fontsize";
+import CustomText from "components/ui/CustomText";
 const Productorder = () => {
   return (
     <View>
       <CustomHeader title="checkout" />
       <ScrollView contentContainerStyle={styles.scrollContainer} >
          <OrderList/>
+         
+        <View style={styles.flexRowBetween}>
+       
+         <View style={styles.flexRow}>
+           <Image source={require('@assets/icons/coupon.png')} style={{width:25, height:25}} />
+           <CustomText style={styles.text}>Use Coupons</CustomText>
+         </View>
+          <Icon name="chevron-right" size={RFValue(16)} style={styles.text} />
+        </View>
+
       </ScrollView>
     </View>
   );
@@ -29,6 +41,19 @@ const styles = StyleSheet.create({
     },
     text:{
         color:'black'
+    },
+    flexRowBetween:{
+       backgroundColor:'#fff',
+       alignItems:'center',
+       justifyContent:'space-between',
+       padding:10,
+       flexDirection:'row',
+       borderRadius:15
+    },
+    flexRow:{
+      alignItems:'center',
+      flexDirection:'row',
+      gap:10
     }
 });
 
