6a21b8c453b9f92384dff0e8125eb28d7110d074
nafis200 <nafisahamed14@gmail.com>
2024-10-23 15:21:09 +0600

    icon container

--- src/features/map/LiveTracking.jsx
+++ src/features/map/LiveTracking.jsx
@@ -1,8 +1,10 @@
 
-import { StyleSheet, Text, View,ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React, { useState } from "react";
 import LiveHeader from "./LiveHeader";
 import Livemap from "./Livemap";
+
+import Icon from 'react-native-vector-icons/Ionicons'
 
 
 const LiveTracking = () => {
@@ -10,30 +12,40 @@ const LiveTracking = () => {
   let msg = "packing your order"
   let time = "arriving in 10 minutes"
 
-  const [currentOrder,setCurrentOrder] = useState('confiremd')
+  const [currentOrder, setCurrentOrder] = useState('confiremd')
 
-  const [person,setPerson] = useState('Customer')
+  const [person, setPerson] = useState('Customer')
 
-  if(currentOrder === 'confiremd'){
-     msg = 'Arriving soon'
-     time = 'Arriving in 8 min'
+  if (currentOrder === 'confiremd') {
+    msg = 'Arriving soon'
+    time = 'Arriving in 8 min'
   }
-  else if(currentOrder === 'arriving'){
-        msg = 'Order Picked up'
-        time = 'arriving in 6 min'
+  else if (currentOrder === 'arriving') {
+    msg = 'Order Picked up'
+    time = 'arriving in 6 min'
   }
-  else if(currentOrder === 'delivered'){
+  else if (currentOrder === 'delivered') {
     msg = 'Order Delivered'
     time = 'Fastest Delivery'
   }
   return (
     <View style={styles.container}>
       <LiveHeader type='Customer' title={msg} secondTitle={time}
-       />
-      
+      />
+
       <ScrollView contentContainerStyle={styles.scrollContent} showsVerticalScrollIndicator={false} >
 
-      <Livemap />
+        <Livemap />
+
+        <View style={styles.flexRow}>
+
+          <View style={styles.iconContainer}>
+
+          <Icon name ='call' color='black' size={30} />    
+
+          </View>
+
+        </View>
 
       </ScrollView>
 
@@ -44,13 +56,19 @@ const LiveTracking = () => {
 export default LiveTracking;
 
 const styles = StyleSheet.create({
-  container:{
-     flex:1,
-     backgroundColor:'green'
+  container: {
+    flex: 1,
+    backgroundColor: 'green'
   },
-  scrollContent:{
+  scrollContent: {
     paddingBottom: 150,
-    backgroundColor:'white',
-    padding:15
-  }
+    backgroundColor: 'white',
+    padding: 15
+  },
+  iconContainer:{
+
+  },
+  flexRow:[
+    
+  ]
 });
